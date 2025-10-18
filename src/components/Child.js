1cdb8a9b40002fe99e68108748b7e42da932890d
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-18 15:31:03 +0530

    Button in Child Changed

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,21 +1,24 @@
-import React from 'react'
-
-function Child({ todos, setTodos, handleComplete }) {
-    return (
-        <div className='m-3.5'>
-            <h2 className='text-2xl mb-1.5'>Child Component</h2>
-            <ul>
-                {todos.map((item) => {
-                    return (
-                        <li key={item.id}>
-                            {item.task}
-                            {item.status ? "" : <button onClick={()=>handleComplete(item.id)} className='bg-blue-400 p-1.5 text-white border-2 border-black cursor-pointer'>Complete</button>}
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+function Child({ todos, handleComplete }) {
+  return (
+    <div className="m-3.5">
+      <h2 className="text-2xl mb-1.5">Child Component</h2>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>
+            {item.task}
+            {item.status ? (
+              <span className="ml-2 text-green-600">(Completed)</span>
+            ) : (
+              <button
+                onClick={() => handleComplete(item.id)}
+                className="bg-blue-400 p-1.5 text-white border-2 border-black cursor-pointer"
+              >
+                Complete
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default Child
